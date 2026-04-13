089a2c59e8e9980b4db0149eda5d21455edf4a87
IconicGuru001 <yboygee2@gmail.com>
2026-04-13 20:29:28 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,51 +1,5 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import "./App.css";
-
 function App() {
-  const [videos, setVideos] = useState([]);
-
-  const fetchVideos = async () => {
-    const res = await axios.get("http://localhost:5000/videos");
-    setVideos(res.data);
-  };
-
-  useEffect(() => {
-    fetchVideos();
-  }, []);
-
-  const likeVideo = async (id) => {
-    await axios.post(`http://localhost:5000/like/${id}`);
-    fetchVideos();
-  };
-
-  return (
-    <div className="app">
-      <div className="header">
-        <h1>KraziWigg</h1>
-      </div>
-
-      <div className="feed">
-        {videos.map((v) => (
-          <div className="videoCard" key={v._id}>
-            <video src={v.url} autoPlay loop muted />
-
-            <div className="overlay">
-              <p className="username">@user</p>
-              <p className="caption">🔥 KraziWigg vibes</p>
-            </div>
-
-            <div className="actions">
-              <button onClick={() => likeVideo(v._id)}>
-                ❤️
-                <span>{v.likes}</span>
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <h1>Hello from Kraziwigg 🚀</h1>;
 }
 
 export default App;
