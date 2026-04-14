7587216815941e7551cbb3bb7971e8b8607fc428
IconicGuru001 <yboygee2@gmail.com>
2026-04-14 04:29:30 +0200

    Add files via upload

--- route.ts
+++ route.ts
@@ -1,20 +1,7 @@
-import cloudinary from "@/lib/cloudinary";
-import Video from "@/models/Video";
 import {connectDB} from "@/lib/db";
+import Video from "@/models/Video";
 import {NextResponse} from "next/server";
-
-export async function POST(req){
+export async function GET(){
 await connectDB();
-const data=await req.formData();
-const file=data.get("file");
-const buffer=Buffer.from(await file.arrayBuffer());
-
-const upload=await new Promise((res,rej)=>{
-cloudinary.uploader.upload_stream({resource_type:"video"},(e,r)=>{
-if(e)rej(e); else res(r);
-}).end(buffer);
-});
-
-const video=await Video.create({url:upload.secure_url});
-return NextResponse.json(video);
+return NextResponse.json(await Video.find().sort({createdAt:-1}));
 }
