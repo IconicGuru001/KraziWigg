1e4f0177a6aaaa6d8ba4dc437a7daf169dfd00de
IconicGuru001 <yboygee2@gmail.com>
2026-04-14 20:28:59 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import {useEffect,useState} from "react";
-export default function Feed(){
-const[videos,setVideos]=useState([]);
-useEffect(()=>{fetch("/api/videos").then(r=>r.json()).then(setVideos)},[]);
-return(<div>{videos.map((v,i)=>
-<video key={i} src={v.url} autoPlay loop controls style={{width:"100%",height:"100vh"}}/>
-)}</div>);
-}
