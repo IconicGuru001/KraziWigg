309e772c1340b6df84e687647fa6b229b19b068c
IconicGuru001 <yboygee2@gmail.com>
2026-04-17 23:52:01 +0200

    Delete backend directory

--- backend/models/User.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-export default mongoose.model("User", new mongoose.Schema({
-username:String,email:String,password:String,followers:[String],following:[String]
-}));
