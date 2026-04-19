d4de2179eb049d5711673cfefcec3ca2a5e6d596
IconicGuru001 <yboygee2@gmail.com>
2026-04-19 23:08:51 +0200

    Delete KraziWigg directory

--- KraziWigg/lib/getUser.js
+++
@@ -1,7 +0,0 @@
-
-import { supabase } from "./supabase";
-
-export async function getUser() {
-  const { data } = await supabase.auth.getUser();
-  return data?.user;
-}
--- KraziWigg/pages/notifications.js
+++
@@ -1,6 +0,0 @@
-
-import NotificationBell from "../components/NotificationBell";
-
-export default function NotificationsPage(){
-  return <NotificationBell userId="USER_ID"/>
-}
